b71fcd2cf246f12bd1f26194d2c4b6fa169512a4
Timo Hyvönen <timo.hyvonen@outlook.com>
2020-07-30 14:58:14 +0300

    Second tutorial video.

---
+++ Quicklauncher/app/src/main/java/com/example/quicklauncher/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.example.quicklauncher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        if(getIntent().hasExtra("org.mentorschools.quicklauncher.SOMETHING")) {
+            TextView tv = (TextView) findViewById(R.id.textView);
+            String text = getIntent().getExtras().getString("org.mentorschools.quicklauncher.SOMETHING");
+            tv.setText(text);
+        }
+    }
+}
